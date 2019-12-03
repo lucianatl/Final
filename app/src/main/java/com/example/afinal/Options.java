714cdedad31fc3b9fa2e14d045bdf3f9216c7d5e
Luciana Toledo-Lopez <toledolu2019@u4sd.org>
2019-12-03 14:20:03 -0600

    second commit

--- app/src/main/java/com/example/afinal/Options.java
+++ app/src/main/java/com/example/afinal/Options.java
@@ -19,53 +19,8 @@ public class Options extends AppCompatActivity {
         final Button Start = findViewById(R.id.Start);
         Start.setOnClickListener(unused -> startActivity(new Intent(this, Flashcards.class)));
     }
-    public void onCheckboxClicked(View view) {
-
-        // Is the view now checked?
-        boolean checked = ((CheckBox) view).isChecked();
-        final CheckBox Pinyin = findViewById(R.id.Pinyin);
-        if (checked && numberOfCheckboxesChecked >= 2) {
-            Pinyin.setChecked(false);
-        } else {
-            if (checked) {
-                numberOfCheckboxesChecked++;
-            } else {
-                numberOfCheckboxesChecked--;
-            }
-        }
+    public void onNextClicked(View view) {
 
 
-        // Check which checkbox was clicked
-        switch(view.getId()) {
-            case R.id.English:
-                if (checked) {
-
-                }
-                // Put some meat on the sandwich
-            else {
-
-                }
-                // Remove the meat
-                break;
-            case R.id.Characters:
-                if (checked) {
-
-                }
-                // Cheese me
-            else
-                // I'm lactose intolerant
-                break;
-            case R.id.Pinyin:
-                if (checked) {
-
-                }
-                // Cheese me
-            else {
-
-                }
-                // I'm lactose intolerant
-                break;
-            // TODO: Veggie sandwich
-        }
     }
 }
